8e51d7e2b9d4940f06c7a6cb2b504f503b9afd5a
Mathias Hansen <me@codemonkey.io>
2013-04-09 15:19:04 -0700

    Student chat

--- app/controllers/CollegeController.php
+++ app/controllers/CollegeController.php
@@ -1,13 +1,19 @@
 <?php
 
 class CollegeController extends BaseController {
+	
+	public function __construct()
+	{
+		$this->beforeFilter('auth');
+	}
 
 	public function showCollege($college_id)
 	{
 		$college = College::find($college_id);
 
 		return View::make('pages.students.college')
-			->with('college', $college);
+			->with('college', $college)
+			->with('user', Auth::user());
 	}
 
 }
