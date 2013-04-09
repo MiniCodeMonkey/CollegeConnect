ad8cd6b4c3111fba475646cf7c771c62aac8fef0
Chris Sevilleja <chris@scotch.io>
2013-04-09 13:47:39 -0700

    student search

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -8,6 +8,7 @@
 		<h2>Your school. Your ambassadors.</h2>
 		<p>Find your college and get familiar by talking to its ambassadors.</p>	
 		<p>They'll help with your college planning, questions, and greatest fears.</p>
+		
 		<a class="btn btn-large btn-primary" href="#">Start Talking to an Ambassador</a>
 		<a id="welcome-ambassador-link" href="#">Be an Ambassador</a>
 	</div>	
