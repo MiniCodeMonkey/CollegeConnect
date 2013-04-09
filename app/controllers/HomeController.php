b6e2679504a30e3f92bee134079755c7c2d9abca
Mathias Hansen <me@codemonkey.io>
2013-04-09 12:24:09 -0700

    Updated home page logic

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,6 +4,23 @@ class HomeController extends BaseController {
 
 	public function showWelcome()
 	{
+		// Is user logged in
+		if (Auth::check())
+		{
+			$user = Auth::user();
+
+			if ($user->user_type == 'AMBASSADOR')
+			{
+				return View::make('pages.ambassadors.index')
+					->with('user', $user);
+			}
+			elseif ($user->user_type == 'STUDENT')
+			{
+				return View::make('pages.students.index')
+					->with('user', $user);
+			}
+		}
+
 		return View::make('pages.index');
 	}
 
