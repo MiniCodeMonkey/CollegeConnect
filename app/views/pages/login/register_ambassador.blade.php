d1ca6fbd69cc736bd1be5e19c5ef7b5d0845ced1
Nicholas Cerminara <nick@scotch.io>
2013-04-09 17:22:59 -0400

    updated form page

--- app/views/pages/login/register_ambassador.blade.php
+++ app/views/pages/login/register_ambassador.blade.php
@@ -3,55 +3,37 @@
 @section('content')
     
 
-	<h1>Welcome Ambassador {{ $user->name }}!</h1>
-	
-	@if (isset($user_profile['education']) && !empty($user_profile['education']))
-		@foreach ($user_profile['education'] as $education)
-			@if ($education['type'] == 'College')
-				
-				
-
-
-				<h2>From {{ $education['school']['name'] }},  {{ $education['year']['name'] }}</h2>
-
-
-
-			@endif
-		@endforeach
-	@endif
-
-
-	<pre>
-	<?php print_r($user_profile); ?>
-	</pre>
-
-	{{ $errors->first('first_name', '<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">×</button><strong>Oh snap!</strong> :message</div>') }}
-	
-
+	<h1>Welcome College Ambassador {{ $user->name }}!</h1>
+	<h3>Which school would you like to be an Ambassador for?</h3>
 
 	{{ Form::open(array('url' => '/login', 'class' => '', 'id' => 'ambassador-form')) }}
-		
-		 <div class="control-group {{ $errors->first('first_name', 'error') }}">
-			<label class="control-label" for="first_name">First Name</label>
-			 <div class="controls">
-			{{
-				Form::text(
-					'first_name',
-					Input::old('first_name'),
-					array(
-						'placeholder' => 'First Name',
-						'class' => $errors->first('first_name', 'error'),
-						'id' => 'first_name'
-					)
-				) }}
-			</div>
+		 <div class="control-group {{ $errors->first('college', 'error') }}">
+				@if (isset($user_profile['education']) && !empty($user_profile['education']))
+					@foreach ($user_profile['education'] as $education)
+						@if ($education['type'] == 'College')
+							
+						<?php
+							$school_name = $education['school']['name'];
+
+							$school_year = '';
+							if (isset($education['year']['name']) && !empty($education['year']['name'])) :
+								$school_year = ', '.$education['year']['name'];
+							endif;
+
+							$study = '';
+							if (isset($education['concentration']) && !empty($education['concentration'])) :
+								$study = ', '.$education['concentration'][0]['name'];
+							endif;
+						?>
+							<div class="controls">
+								<label class="radio">{{ Form::radio('college', 1); }} {{ $school_name.$study.$school_year }}</label>
+							</div>
+
+						@endif
+					@endforeach
+				@endif
 		 </div>
-		
-
-		{{ Form::submit('Submit') }}
-	
-
+		 <button type="submit" class="btn">Begin Helping Students</button>
 	{{ Form::close() }}
-
 
 @stop
