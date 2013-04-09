31d464717ed2a74a0dbda2a65390c87ba67bd023
Mathias Hansen <me@codemonkey.io>
2013-04-08 20:55:12 -0400

    Added Facebook config file

---
+++ app/config/development/facebook.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+
+	'app_id' => '503540799706055',
+	'app_secret' => '850d12d508cc6b08addde782986dbf6d',
+
+);
---
+++ app/config/facebook.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+
+	'app_id' => '125848477606968',
+	'app_secret' => '72b7f6f2dcab36b7ce56b95ee4036880',
+
+);
