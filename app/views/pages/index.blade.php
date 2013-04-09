dd75641197c3ca621203877c9deb2027c0ce3617
Chris Sevilleja <chris@scotch.io>
2013-04-09 13:13:09 -0700

    home page

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -8,8 +8,8 @@
 		<h2>Your school. Your ambassadors.</h2>
 		<p>Find your college and get familiar by talking to its ambassadors.</p>	
 		<p>They'll help with your college planning, questions, and greatest fears.</p>
-		<a class="btn btn-large clearfix" href="#">Start Talking to an Ambassador</a>
-		<a class="btn btn-small clearfix" href="#">Be an Ambassador</a>
+		<a class="btn btn-large btn-primary" href="#">Start Talking to an Ambassador</a>
+		<a id="welcome-ambassador-link" href="#">Be an Ambassador</a>
 	</div>	
 </div>
 @stop
