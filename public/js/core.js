6000fea576568664f4556ba8695e4f59d3e5a3c8
Chris Sevilleja <chris@scotch.io>
2013-04-09 12:25:19 -0700

    added subpage layout and home page changes

--- public/js/core.js
+++ public/js/core.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	var map;
+  	var map;
       function initialize() {
         var mapOptions = {
           zoom: 8,
-          center: new google.maps.LatLng(37.476437,-122.155075),
+          center: new google.maps.LatLng(37.583766,-123.563232),
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -11,4 +11,8 @@ $(document).ready(function() {
       }
 
       google.maps.event.addDomListener(window, 'load', initialize);
+
+      var height = $(window).height();
+      height = height - 100;
+      $('#map-canvas').height(height + 'px');
 });
