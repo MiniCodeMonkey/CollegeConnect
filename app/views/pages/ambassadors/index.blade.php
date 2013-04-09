5b1091d6f6d80626118b4ec4605b6e3a071265a7
Mathias Hansen <me@codemonkey.io>
2013-04-09 12:25:58 -0700

    Added home pages for ambassadors and students

---
+++ app/views/pages/ambassadors/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/main')
+
+@section('content')
+<div id="welcome">
+    <div id="welcome-msg">
+		<h1>You are logged in as an ambassador!</h1>
+		<p>Hold on to get connected with students who want to learn more about your college.</p>
+	</div>	
+</div>
+@stop
