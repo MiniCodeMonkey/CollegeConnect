586c4e4e7d3f19e25f5f9a512d9076595c6eb483
Mathias Hansen <me@codemonkey.io>
2013-04-09 11:53:04 -0700

    Login controller

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -2,9 +2,29 @@
 
 class LoginController extends BaseController {
 
+	public function __construct()
+	{
+		$this->beforeFilter('auth.fb'); // Make sure that we are Facebook authenticated
+	}
+
 	public function getIndex()
 	{
-		
+		$facebook = new Facebook(array(
+		  'appId'  => Config::get('facebook.app_id'),
+		  'secret' => Config::get('facebook.app_secret'),
+		));
+
+		$fbUserProfile = $facebook->api('/me');
+			
+		$user = User::fromFacebookId($fbUserProfile->id);
+
+		if (!$user) {
+			$user = new User;
+			$user->name = $fbUserProfile->name;
+			$user->fb_id = $fbUserProfile->id;
+			$user->fb_accesstoken = $facebook->getAccessToken();
+			$user->save();
+		}
 	}
 
 }
